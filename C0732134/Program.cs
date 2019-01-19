a02767878431e65622bbe4594683c56bcc134807
hpsnannar@outlook.com <hpsnannar@outlook.com>
2019-01-18 19:06:51 -0500

    First Commit

---
+++ C0732134/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C0732134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
