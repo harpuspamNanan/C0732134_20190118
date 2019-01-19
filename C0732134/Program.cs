1fd4577a07b59647e7a87fa75ad7b904f5a04e2b
hpsnannar@outlook.com <hpsnannar@outlook.com>
2019-01-18 19:52:56 -0500

    Creating an Object for Different data types.

--- C0732134/Program.cs
+++ C0732134/Program.cs
@@ -6,9 +6,36 @@ namespace C0732134
     {
         static void Main(string[] args)
         {
-            int num = 5;
-            int sum = num + 5;
-            Console.WriteLine(" "+sum);
+            DataSet ds = new DataSet();
+            bool cond = true;
+            while (cond)
+            {
+                Console.WriteLine("Enter the name: ");
+                ds.name = Console.ReadLine();
+                Console.WriteLine("Enter the GPA: ");
+                ds.gpa = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the Grade in Integer : ");
+                ds.grade = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Do you wish to Continue press [y, n]");
+                if (Console.ReadLine() != "y")
+                    cond = false;
+            }
+
+            for (int i = 0; i <  ; i++)
+            {
+
+            }
+            Console.WriteLine("\n\nName : "+ ds.name);
+            Console.WriteLine("\n\nName : " + ds.gpa);
+            Console.WriteLine("\n\nName : " + ds.grade);
         }
+    }
+
+    class DataSet
+    {
+       public string name = "";
+       public double gpa = 0.0f;
+       public int grade = 0;
     }
 }
